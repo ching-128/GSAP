6d607914201b33a4d8337f7009e7117c98a3b69a
Prasad <prasadgowda@turiya.co>
2025-07-05 18:12:41 +0530

    Refactor HTML and JavaScript to implement text animation on specified elements

--- Phase1/splitText/script.js
+++ Phase1/splitText/script.js
@@ -1,6 +1,7 @@
-gsap.to("p", {
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.", duration: 3,
-    ease: "power1",
+gsap.to(".text1", {
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    duration: 3,
+      ease: "power1",
     repeat: 1,
     yoyo: true,
 })
